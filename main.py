856e12f968ce7fe2b1a15bd325629d185d2d5a68
Lucas Cantoni <lukcnt@outlook.com>
2022-07-27 01:10:47 -0300

    User tip percentage choice.

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print("Welcome to the tip calculator.")
 
 #Total bill amount input from the user.
 total_bill = input("What was the total bill: $")
+
+#Percentage of the tip.
+percentage = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
+tip_percentage = percentage / 100
