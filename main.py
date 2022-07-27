a44c6e311735a726519e875da08aad05ab0c2e66
Lucas Cantoni <lukcnt@outlook.com>
2022-07-27 01:14:41 -0300

    Input of the number of how many people will split the bill.

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ total_bill = input("What was the total bill: $")
 #Percentage of the tip.
 percentage = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
 tip_percentage = percentage / 100
+
+#Number of people who will split the bill.
+number_people = int(input("How many people to split the bill? "))
